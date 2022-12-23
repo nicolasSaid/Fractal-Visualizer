40cbe74b78e0a0f6095d6c41a04075a7e9d06dc2
Nicolas Said <nicolassaid@nicolass-mbp.myfiosgateway.com>
2022-12-23 00:59:44 -0500

    Implemented calculations for the recursive triangles that make up Sierpinski's triangle

--- Sierpinski.java
+++ Sierpinski.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 public class Sierpinski implements Fractal{
-    ArrayList<ArrayList<Triangle>> layers;
+    ArrayList<Triangle> layers;
     private int iterations;
     private double zoom;
 
@@ -28,5 +30,24 @@ public class Sierpinski implements Fractal{
 
     public double getZoom(){
         return zoom;
+    }
+
+    private void recursiveCalcTriangles(int x, int y, int length, int n){
+        if(n == 0){
+            return;
+        }
+        layers.add(new Triangle(x,y,length,false));
+
+        recursiveCalcTriangles(x + x/2, y/2, length/2, n - 1);
+        recursiveCalcTriangles(x/2, y + y/2, length/2, n - 1);
+        recursiveCalcTriangles(2*x + x/2, y + y/2, length/2, n - 1);
+    }
+
+    public void calcTriangles(){
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int startCorner = (int)((screenSize.getWidth() - screenSize.getHeight())/2);
+        layers.add(new Triangle(startCorner,(int)(screenSize.getHeight()), (int)(screenSize.getHeight()), true));
+        recursiveCalcTriangles((int)(screenSize.getHeight()/4),(int)(screenSize.getHeight()/2),(int)(screenSize.getHeight()/2),iterations);
+        return;
     }
 }
