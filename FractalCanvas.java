b9cdded6d90e3a24790c05b04bed5d26c83fd221
Nicolas Said <nicolassaid@Nicolass-MacBook-Pro.local>
2023-02-05 02:26:18 -0500

    implemented color menu item and deleted unused imports in fractalDraw

--- FractalCanvas.java
+++ FractalCanvas.java
@@ -4,14 +4,18 @@ import java.awt.Graphics;
 
 public class FractalCanvas extends Canvas{
     private int fractalIterations;
+    private Color drawingColor;
+    private Color backgroundColor;
 
-    public FractalCanvas(int i){
+    public FractalCanvas(int i, Color col, Color colBack){
         fractalIterations = i;
+        drawingColor = col;
+        backgroundColor = colBack;
     }
 
     public void paint(Graphics g){
-        setForeground(Color.BLACK);
-        setBackground(Color.WHITE);
+        setForeground(drawingColor);
+        setBackground(backgroundColor);
         Sierpinski outputSierpinski = new Sierpinski(fractalIterations);
         outputSierpinski.drawFractal(g);
         //g.drawLine(0,0,Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
--- fractalDraw.java
+++ fractalDraw.java
@@ -1,29 +1,11 @@
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.Insets;
-import java.awt.Rectangle;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Canvas;
-import java.awt.Graphics;
 import java.awt.Toolkit;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.TreeSet;
-import java.util.Collections;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.io.FileInputStream;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -35,7 +17,6 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.KeyStroke;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.border.Border;
@@ -43,7 +24,10 @@ import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 
+
 public class fractalDraw{
+    private Color drawingColor;
+    private Color backgroundColor;
 
     public static void main(String[] args){
         SwingUtilities.invokeLater(new Runnable() {
@@ -148,7 +132,7 @@ public class fractalDraw{
 
     public void showFractal(int i, JFrame prev){
         JFrame frame = new JFrame();
-        FractalCanvas f = new FractalCanvas(i);
+        FractalCanvas f = new FractalCanvas(i,drawingColor,backgroundColor);
         addMenuBar(frame);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(f);
@@ -175,11 +159,11 @@ public class fractalDraw{
         layoutPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         //JPanel promptPanel = new JPanel();
-        JLabel prompt = new JLabel("<html>TEMP</html>");
+        JLabel prompt = new JLabel("<html><strong>About the Programmer: </strong><em>Nicolas Said</em></html>");
         //promptPanel.setPreferredSize(new Dimension(50,10));
         prompt.setVerticalAlignment(JLabel.TOP);
         prompt.setHorizontalAlignment(JLabel.CENTER);
-        prompt.setFont(new Font("Verdana", Font.BOLD, 36));
+        prompt.setFont(new Font("Verdana", Font.PLAIN, 36));
         prompt.setBorder(new EmptyBorder(50,0,20,0));
         /*Border border = prompt.getBorder();
         Border margin = new EmptyBorder(100,100,0,100);
@@ -187,20 +171,20 @@ public class fractalDraw{
         layoutPanel.add(prompt);
 
         //JPanel descPanel = new JPanel();
-        JLabel desc = new JLabel("<html>TEMP DESC<br>TEMP DESC<br>TEMP DESC<br>TEMP DESC<br>TEMP DESC<br>TEMP DESC</html>");
+        JLabel desc = new JLabel("<html><strong>Education: </strong>Sophomore at Binghamton University<br><strong>Involvement: </strong>Association for Computing Machinery and HackBU<br><strong>Programming Languages: </strong>Java, C/C++, Visual Basics, HTML, CSS</html>");
      
         desc.setVerticalAlignment(JLabel.TOP);
         desc.setHorizontalAlignment(JLabel.CENTER);
         desc.setFont(new Font("Verdana", Font.PLAIN, 14));
-        desc.setBorder(new EmptyBorder(0,0,475,0));
 
         layoutPanel.add(desc);
 
 
-        JLabel link = new JLabel("<html>link placeholder</html>");
+        JLabel link = new JLabel("<html><strong>Linkedin: </strong>www.linkedin.com/in/nicolasleesaid<br><strong>Github: </strong>https://github.com/nicolasSaid<br><strong>Hackerrank: </strong>https://www.hackerrank.com/nicolasleesaid</html>");
         link.setVerticalAlignment(JLabel.BOTTOM);
         link.setHorizontalAlignment(JLabel.CENTER);
         link.setFont(new Font("Verdana", Font.PLAIN, 10));
+        link.setBorder(new EmptyBorder(100,0,0,0));
 
         layoutPanel.add(link);
 
@@ -212,6 +196,11 @@ public class fractalDraw{
         frame.setVisible(true);
         prev.dispose();
     }
+
+    /*public void createPaintingCanvas(JFrame prev){
+        JFrame frame = new JFrame();
+
+    }*/
 
 
     class ButtonActionListener implements ActionListener {
@@ -258,6 +247,109 @@ public class fractalDraw{
             else if(mi.getText().toLowerCase().equals("about me")) {
                 showAboutMe(current);
 			}
+            else if(mi.getText().toLowerCase().equals("color")) {
+                boolean correctInput = false;
+                String input;
+                while(!correctInput){
+                    input =JOptionPane.showInputDialog(null, "Enter a Color to change the drawing color or \"cancel\"");
+                    correctInput = true;
+                    if(input.toLowerCase().equals("black")){
+                        drawingColor = Color.BLACK;
+                    } else if(input.toLowerCase().equals("blue")){
+                        drawingColor = Color.BLUE;
+                    }
+                    else if(input.toLowerCase().equals("cyan")){
+                        drawingColor = Color.CYAN;
+                    }
+                    else if(input.toLowerCase().equals("dark gray") || input.toLowerCase().equals("dark grey")){
+                        drawingColor = Color.DARK_GRAY;
+                    }
+                    else if(input.toLowerCase().equals("gray") || input.toLowerCase().equals("grey")){
+                        drawingColor = Color.GRAY;
+                    }
+                    else if(input.toLowerCase().equals("light gray") || input.toLowerCase().equals("light grey")){
+                        drawingColor = Color.LIGHT_GRAY;
+                    }
+                    else if(input.toLowerCase().equals("magenta")){
+                        drawingColor = Color.MAGENTA;
+                    }
+                    else if(input.toLowerCase().equals("orange")){
+                        drawingColor = Color.ORANGE;
+                    }
+                    else if(input.toLowerCase().equals("pink")){
+                        drawingColor = Color.PINK;
+                    }
+                    else if(input.toLowerCase().equals("red")){
+                        drawingColor = Color.RED;
+                    }
+                    else if(input.toLowerCase().equals("white")){
+                        drawingColor = Color.WHITE;
+                    }
+                    else if(input.toLowerCase().equals("green")){
+                        drawingColor = Color.GREEN;
+                    }
+                    else if(input.toLowerCase().equals("yellow")){
+                        drawingColor = Color.YELLOW;
+                    }
+                    else if(input.toLowerCase().equals("cancel")){}
+                    else{
+                        JOptionPane.showMessageDialog(null, "Please enter a valid input");
+                        correctInput = false;
+                    }
+                }
+                correctInput = false;
+                while(!correctInput){
+                    input =JOptionPane.showInputDialog(null, "Enter a Color to change the background color or \"cancel\"");
+                    correctInput = true;
+                    if(input.toLowerCase().equals("black")){
+                        backgroundColor = Color.BLACK;
+                    } else if(input.toLowerCase().equals("blue")){
+                        backgroundColor = Color.BLUE;
+                    }
+                    else if(input.toLowerCase().equals("cyan")){
+                        backgroundColor = Color.CYAN;
+                    }
+                    else if(input.toLowerCase().equals("dark gray") || input.toLowerCase().equals("dark grey")){
+                        backgroundColor = Color.DARK_GRAY;
+                    }
+                    else if(input.toLowerCase().equals("gray") || input.toLowerCase().equals("grey")){
+                        backgroundColor = Color.GRAY;
+                    }
+                    else if(input.toLowerCase().equals("light gray") || input.toLowerCase().equals("light grey")){
+                        backgroundColor = Color.LIGHT_GRAY;
+                    }
+                    else if(input.toLowerCase().equals("magenta")){
+                        backgroundColor = Color.MAGENTA;
+                    }
+                    else if(input.toLowerCase().equals("orange")){
+                        backgroundColor = Color.ORANGE;
+                    }
+                    else if(input.toLowerCase().equals("pink")){
+                        backgroundColor = Color.PINK;
+                    }
+                    else if(input.toLowerCase().equals("red")){
+                        backgroundColor = Color.RED;
+                    }
+                    else if(input.toLowerCase().equals("white")){
+                        backgroundColor = Color.WHITE;
+                    }
+                    else if(input.toLowerCase().equals("green")){
+                        backgroundColor = Color.GREEN;
+                    }
+                    else if(input.toLowerCase().equals("yellow")){
+                        backgroundColor = Color.YELLOW;
+                    }
+                    else if(input.toLowerCase().equals("cancel")){}
+                    else{
+                        JOptionPane.showMessageDialog(null, "Please enter a valid input");
+                        correctInput = false;
+                    }
+                }
+			}
+            else if(mi.getText().toLowerCase().equals("paint")) {
+                //think on this :D
+            }
+
 		}
 	}
 }
